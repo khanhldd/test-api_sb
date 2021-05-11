62910fc33b99ea8c5d9cfb68506340f6a80d5589
khanhldd <daigunder12331@gmail.com>
2021-05-11 15:59:03 +0700

    first commit

---
+++ src/main/java/com/khanhldd/api/NewAPI.java
@@ -0,0 +1,30 @@
+package com.khanhldd.api;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.khanhldd.dto.NewDTO;
+
+@RestController
+public class NewAPI {
+	/* @RequestMapping(value = "/new", method = RequestMethod.POST) */
+	@PostMapping(value = "/new")
+	public NewDTO createNew(@RequestBody NewDTO model) {
+		return model;
+	}
+	
+	@PutMapping(value = "new")
+	
+	public NewDTO updateNew(@RequestBody NewDTO model) {
+		return model;
+	}
+	
+	@DeleteMapping(value = "new")
+	public void deleteNew(@RequestBody Long[] ids) {
+		
+	}
+
+}
